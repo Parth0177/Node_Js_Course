fa336814b13a99df8ecc2667eab516e509100bb2
Parth Tiwari <parthtiwari670@gmail.com>
2025-04-21 08:33:16 +0530

    Add WriteFile.js and output.txt for file writing functionality

---
+++ WriteFile.js
@@ -0,0 +1,15 @@
+const fs= require('fs');
+
+console.log("Hello, World!");
+
+fs.writeFile("output.txt","Writing File",(err)=>{
+  if(err){
+    console.log("Error Occured");
+  }
+  else {
+    console.log("File Created Successfully");
+  }
+});
+
+//File writing is available in node js...
+  
