f996281af3e6c0d2fe9f0e92c25e08e193f81cc9
Parth Tiwari <parthtiwari670@gmail.com>
2025-05-05 01:35:59 +0530

    Add form handling and user data storage in server implementation

--- lecture-4/userInput.js
+++ lecture-4/userInput.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log(req.url,req.method,req.headers);
@@ -6,23 +7,32 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>This is my Home Page</title></head>')
-    res.write('<body><h1>Hello! This is my first Node server response.</h1></body>')
-    res.write('<body><p>Hello this is home page.</p></body>')
+    res.write('<body><h1>Hello! Enter Your Details</h1>');
+    res.write('<form action="/submit-details" method="POST">');
+    res.write('<input type="text" name="username" placeholder="Enter your name"><br><br>');
+    res.write('<label for="gender">Gender</label>');
+    res.write('<input type="radio" id="male" name="gender value="male">');
+    res.write('<label for="male">Male</label>');
+    res.write('<input type="radio" id="female" name="gender value="female">');
+    res.write('<label for="male">Female</label><br><br>');
+    res.write('<button type="submit">Submit</button>')
+    res.write('</form>');
+    res.write('</body>');
     res.write('</html>');
     return res.end();
-  }else if (req.url==='/form'){
-    res.write('<h1>Welcome to Home Page</h1>');
-    res.write('<form action="/submit-details" method="POST">');
-    res.write('<input type="text" id="name" name="username" placeholder="Enter your name"><br><br>');
-    res.write('<label for="gender">Gender:</label>');
-    res.write('<input type="Radio" id="male" name="gender" value="male">');
-    res.write('<label for="male">Male</label>');
-    res.write('<input type="Radio" id="female" name="gender" value="female">');
-    res.write('<label for="female">Female</label><br><br>');
-    res.write('<button type="submit">Submit</button>');
-    res.write('</form>');
-    return res.end();
-  }
+    }else if(req.url.toLowerCase() ==='/submit-details'&& req.method=='POST' ){
+      fs.writeFileSync('user.txt', 'Parth Tiwari');
+      res.statusCode = 302;
+      res.setHeader('Location','/');
+    }
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<html>');
+      res.write('<head><title>  My First Page</title></head>');
+      res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+      res.write('<body><p>Hello My Name is Parth!</p></body>')
+      res.write('</html>'); 
+      res.end(); 
+    
     
 });
 
